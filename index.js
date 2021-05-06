f00bfa28c01a4ef189cab83662dc3bc1ddc61a8b
dkvema <83395777+dkvema@users.noreply.github.com>
2021-05-06 17:09:04 +0800

    Update index.js

--- index.js
+++ index.js
@@ -1,34 +1,26 @@
-const secrets = require("secrets_getsecretvalue.js");
-const express = require('express');
-const path = require('path');
-const cookieParser = require('cookie-parser');
-const bodyParser = require('body-parser');
-const routes = require('./routes/index');
-const secrets = require('aws-secrets');
-const app = express();
-
-// View enginer setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'hbs');
-
-// Middleware setup
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
-
-// Mount routes
-app.use('/', routes);
-
-//added newly devenda start
-const dotenv = require('dotenv');
-dotenv.config();
-console.log(`Your port is ${process.env.PORT}`); // 8626
-
-//added newly devenda end
-
-// Start server
-var port = process.env.PORT || '3000';
-app.set('port', port);
-app.listen(port, function () {
-  console.log('Example app listening on port ' + port);
-});
+const {loadSecrets} = require('./secrets.js');
+loadSecrets()
+  .then(()=>{
+    const express = require('express');
+    const path = require('path');
+    const cookieParser = require('cookie-parser');
+    const bodyParser = require('body-parser');
+    const routes = require('./routes/index');
+    const app = express();
+    // View enginer setup
+    app.set('views', path.join(__dirname, 'views'));
+    app.set('view engine', 'hbs');
+    // Middleware setup
+    app.use(bodyParser.urlencoded({ extended: false }));
+    app.use(cookieParser());
+    app.use(express.static(path.join(__dirname, 'public')));
+    // Mount routes
+    app.use('/', routes);
+    //added newly devenda end
+    // Start server
+    var port = process.env.PORT || '3000';
+    app.set('port', port);
+    app.listen(port, function () {
+      console.log('Example app listening on port ' + port);
+    });
+  });
