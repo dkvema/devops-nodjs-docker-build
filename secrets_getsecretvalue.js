0b65d0db1fb6af9f017b698b8ed95224bbc3a40c
dkvema <83395777+dkvema@users.noreply.github.com>
2021-05-06 22:19:32 +0800

    Update secrets_getsecretvalue.js

--- secrets_getsecretvalue.js
+++ secrets_getsecretvalue.js
@@ -41,7 +41,7 @@ function loadSecrets() {
             // Depending on whether the secret is a string or binary, one of these fields will be populated.
             if ('SecretString' in data) {
                 secret = data.SecretString;
-                console.log("Secrets from aws for DATA_FILE", dataFile);
+                console.log("Secrets from aws for secrets_get_secretvalue",secret);
             } else {
                 let buff = new Buffer(data.SecretBinary, 'base64');
                 decodedBinarySecret = buff.toString('ascii');
