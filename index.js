e7ed3b31c9a1f97f2483621d3337bda6c40ed3f1
dkvema <83395777+dkvema@users.noreply.github.com>
2021-05-07 11:27:09 +0800

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const {loadSecrets} = require('./secrets_getsecretvalue.js');
 loadSecrets()
   .then(()=>{
+    console.log(process.env);
     const express = require('express');
     const path = require('path');
     const cookieParser = require('cookie-parser');
