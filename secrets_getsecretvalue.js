8cc8807ad760d8169b033f96d5bff0915b497714
dkvema <83395777+dkvema@users.noreply.github.com>
2021-05-07 23:35:11 +0800

    Update secrets_getsecretvalue.js

--- secrets_getsecretvalue.js
+++ secrets_getsecretvalue.js
@@ -44,6 +44,7 @@ function loadSecrets() {
             if ('SecretString' in data) {
                 secret = data.SecretString;
                Object.assign(process.env, JSON.parse(secret))
+                console.log("Secrets from jenkins",process.env);
             } else {
                 let buff = new Buffer(data.SecretBinary, 'base64');
                 decodedBinarySecret = buff.toString('ascii');
