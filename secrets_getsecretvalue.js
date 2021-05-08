236aa73cc156258dfc6b8039a6018af731a0816d
dkvema <83395777+dkvema@users.noreply.github.com>
2021-05-08 09:33:32 +0800

    Update secrets_getsecretvalue.js

--- secrets_getsecretvalue.js
+++ secrets_getsecretvalue.js
@@ -4,7 +4,8 @@ var AWS = require('aws-sdk'),
      region="ap-southeast-1",
     //secretName = ‘${environment}/my-secrets’,
     //secretName = `${environment}/my-secrets`,
-    secretName = `${process.env.ENVIRONMENT}/my-secrets`,
+   // secretName = "nodejs-docker-secrets",
+   secretName = `${process.env.ENVIRONMENT}/my-secrets`,
     secret,
     decodedBinarySecret;
     var client = new AWS.SecretsManager({
